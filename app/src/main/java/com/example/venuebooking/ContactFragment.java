ea6e2d58864dcf8f124676efb9069a678d90ae2b
sudeepnn <sudeep2002naik@gmail.com>
2023-06-09 16:57:57 +0530

    duplicate class alternative

--- app/src/main/java/com/example/venuebooking/ContactFragment.java
+++ app/src/main/java/com/example/venuebooking/ContactFragment.java
@@ -1,10 +1,15 @@
 package com.example.venuebooking;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class ContactFragment extends Fragment {
@@ -12,7 +17,56 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+        CardView whtsapp=view.findViewById(R.id.whtsapp);
+        CardView gmail=view.findViewById(R.id.mail);
+        CardView phone=view.findViewById(R.id.phone);
+        CardView location=view.findViewById(R.id.location);
+        whtsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String wurl="https://wa.me/+917349693447?text=hiii";
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(wurl));
+                startActivity(intent);
+            }
+        });
+
+        gmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String recipient = "sudeep2002naik@gmail.com";
+                String subject = "Hello";
+                String body = "This is the body of the email.";
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{recipient});
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                intent.putExtra(Intent.EXTRA_TEXT, body);
+                startActivity(intent);
+            }
+        });
+
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumber = "7349693447";
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                startActivity(intent);
+            }
+        });
+
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double latitude = 12.866773099716804;
+                double longitude = 74.92538832883592;
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:" + latitude + "," + longitude));
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/venuebooking/adapters/VenueAdpaters.java
@@ -0,0 +1,55 @@
+package com.example.venuebooking.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.venuebooking.R;
+import com.example.venuebooking.models.VenueModel;
+
+import java.util.List;
+
+public class VenueAdpaters extends RecyclerView.Adapter<VenueAdpaters.ViewHolder> {
+    private Context context;
+    private List<VenueModel> venueModelList;
+
+    public VenueAdpaters(Context context, List<VenueModel> venueListList) {
+        this.context = context;
+        this.venueModelList = venueListList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.venue_list,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Glide.with(context).load(venueModelList.get(position).getImg_url()).into(holder.venue_img);
+        holder.venue_name.setText(venueModelList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return venueModelList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView venue_img;
+        TextView venue_name;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            venue_img = itemView.findViewById(R.id.venue_img);
+            venue_name = itemView.findViewById(R.id.venue_name);
+        }
+    }
+}
