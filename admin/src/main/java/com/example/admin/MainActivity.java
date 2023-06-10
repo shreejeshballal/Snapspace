a197e99d32a6f00ed2e3aa8bee5c362a292f1070
sudeepnn <sudeep2002naik@gmail.com>
2023-06-10 17:26:36 +0530

    admin app

--- admin/src/main/java/com/example/admin/MainActivity.java
+++ admin/src/main/java/com/example/admin/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.admin;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
+import com.example.admin.Fragments.Admin_home;
+import com.example.admin.Fragments.Payments;
+import com.example.admin.Fragments.Reservation;
 import com.example.admin.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,5 +26,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         frameLayout=(FrameLayout)findViewById(R.id.framelayout);
         bottomNavigationView=(BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        getSupportFragmentManager().beginTransaction().add(R.id.framelayout,new Admin_home()).commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new Admin_home()).commit();
+                        break;
+                    case R.id.register:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new Reservation()).commit();
+                        break;
+                    case R.id.contact:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new Payments()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
     }
-}
+
+    public void logout(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to logout ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+    }
+    }
