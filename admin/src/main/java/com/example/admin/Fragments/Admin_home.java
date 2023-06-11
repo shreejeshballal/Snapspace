0f4bd751c867045db1a7e721072bb0881870ea84
sudeepnn <sudeep2002naik@gmail.com>
2023-06-11 16:09:42 +0530

    booking page

--- admin/src/main/java/com/example/admin/Fragments/Admin_home.java
+++ admin/src/main/java/com/example/admin/Fragments/Admin_home.java
@@ -3,103 +3,70 @@ package com.example.admin.Fragments;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.Toast;
 
-import com.example.admin.Models.Venuelist;
+import com.example.admin.Models.VenueModel;
 import com.example.admin.R;
-import com.example.admin.adapters.Venuelistadapter;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
+import com.example.admin.adapters.VenueAdpaters;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import org.w3c.dom.Document;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Admin_home extends Fragment {
-    RecyclerView recyclerView;
-    ArrayList<Venuelist> venuelistArrayList;
-    Venuelistadapter adVenuelistadapter;
+    RecyclerView venueRecycler;
     FirebaseFirestore db;
-
-    ProgressDialog progressDialog;
-
+    ArrayList<VenueModel> venueModelList;
+    VenueAdpaters venueAdpaters;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
-        progressDialog=new ProgressDialog(getContext());
-        progressDialog.setCancelable(false);
-        progressDialog.setMessage("fetching .....");
-        progressDialog.show();
+        View root = inflater.inflate(R.layout.fragment_admin_home, container, false);
+        db = FirebaseFirestore.getInstance();
 
+        venueRecycler = root.findViewById(R.id.venuelist);
 
-        View view = inflater.inflate(R.layout.fragment_admin_home, container, false);
-        recyclerView=view.findViewById(R.id.venuelist);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        venueRecycler.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        venueModelList = new ArrayList<>();
+        venueAdpaters = new VenueAdpaters(getActivity(),venueModelList,venueRecycler);
+        venueRecycler.setAdapter(venueAdpaters);
 
-        db=FirebaseFirestore.getInstance();
-        venuelistArrayList=new ArrayList<>();
-        adVenuelistadapter=new Venuelistadapter(getContext(),venuelistArrayList);
-        recyclerView.setAdapter(adVenuelistadapter);
+        db.collection("venues")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
 
-        eventchange();
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                VenueModel venueModel = document.toObject(VenueModel.class);
+                                venueModelList.add(venueModel);
+                                venueAdpaters.notifyDataSetChanged();
 
-        return view;
+                            }
+                        } else {
+                            Exception exception = task.getException();
+                            if (exception != null) {
+                                Toast.makeText(getActivity(), "Error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getActivity(), "Error occurred", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
 
-
+        return root;
     }
-
-    private void eventchange() {
-        db.collection("venues").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                List<DocumentSnapshot> veList=queryDocumentSnapshots.getDocuments();
-                for(DocumentSnapshot ds:veList){
-                    venuelistArrayList.add(ds.toObject(Venuelist.class));
-
-                }
-            }
-        });
-    }
-
-//    private void eventchange() {
-//
-//        db.collection("venues").addSnapshotListener(new EventListener<QuerySnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-//                if(error !=null){
-//                    if(progressDialog.isShowing())
-//                        progressDialog.dismiss();
-//                    Log.e("Firebase error",error.getMessage());
-//                    return;
-//                }
-//                for(DocumentChange dc:value.getDocumentChanges()){
-//                    if(dc.getType()== DocumentChange.Type.ADDED){
-//                        venuelistArrayList.add(dc.getDocument().toObject(Venuelist.class));
-//                    }
-//                    adVenuelistadapter.notifyDataSetChanged();
-//                    if(progressDialog.isShowing())
-//                        progressDialog.dismiss();
-//                }
-//            }
-//        });
-//    }
 }
---
+++ app/src/main/java/com/example/venuebooking/ui/Venue_booking.java
@@ -0,0 +1,16 @@
+package com.example.venuebooking.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.venuebooking.R;
+
+public class Venue_booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venue_booking);
+    }
+}
