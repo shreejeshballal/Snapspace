514d6d3af5cc9df67da90a62624c60716f57b9d4
Shreejesh J Ballal <shreejeshballal@gmail.com>
2023-06-11 08:01:08 +0530

    Recycler View

--- app/src/main/java/com/example/venuebooking/fragments/HomeFragment.java
+++ app/src/main/java/com/example/venuebooking/fragments/HomeFragment.java
@@ -1,21 +1,70 @@
 package com.example.venuebooking.fragments;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.venuebooking.R;
+import com.example.venuebooking.adapters.VenueAdpaters;
+import com.example.venuebooking.models.VenueModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.ktx.Firebase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
-
+    RecyclerView venueRecycler;
+    FirebaseFirestore db;
+    ArrayList<VenueModel> venueModelList;
+    VenueAdpaters venueAdpaters;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        db = FirebaseFirestore.getInstance();
+
+        venueRecycler = root.findViewById(R.id.venue_recyler);
+
+        venueRecycler.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        venueModelList = new ArrayList<>();
+        venueAdpaters = new VenueAdpaters(getActivity(),venueModelList,venueRecycler);
+        venueRecycler.setAdapter(venueAdpaters);
+
+        db.collection("venues")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                            VenueModel venueModel = document.toObject(VenueModel.class);
+                venueModelList.add(venueModel);
+                venueAdpaters.notifyDataSetChanged();
+
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Error",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        return root;
     }
 }
